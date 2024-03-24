a7149bdc6746c356e83b864d29e10bfd15cd70db
NineTears <hellowind@live.com>
2024-03-24 19:21:21 +0800

    更新 AceAddon-2.0.lua

--- Libs/AceAddon/AceAddon-2.0.lua
+++ Libs/AceAddon/AceAddon-2.0.lua
@@ -248,7 +248,7 @@ elseif GetLocale() == "zhCN" then
 	COMMANDS = "命令"
 	
 	ABOUT = "关于"
-	PRINT_ADDON_INFO = "印列出插件信息"
+	PRINT_ADDON_INFO = "打印插件信息"
 	
 	CATEGORIES = {
 		["Action Bars"] = "动作条",
@@ -404,8 +404,8 @@ else -- enUS
 	CREDITS = "Credits"
 	COMMANDS = "Commands"
 	
-	ABOUT = "About"
-	PRINT_ADDON_INFO = "Print out addon info"
+	ABOUT = "关于"
+	PRINT_ADDON_INFO = "打印插件信息"
 	
 	CATEGORIES = {
 		["Action Bars"] = "Action Bars",
